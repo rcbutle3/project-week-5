d8e283abdb200d422a083fe59e4144e4fa10211f
rcbutle3 <Robin@MR01_Blackwell_ATC.car01.drhm.nc.frontiernet.net>
2017-10-30 14:13:33 -0400

    second commit

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
+let main = document.querySelector(".resultlist")
+let search = document.querySelector(".searchBar")
+let url = "https://itunes.apple.com/search?term="
+search.addEventListener('keyup', function(e) {
 
-// 1. First select and store the elements you'll be working with
-let mainbutton = document.getElementById('mainbuttonButton');
-let url = 'https://itunes.apple.com/search?';
+  let term = search.value;
+  fetch(`https://itunes.apple.com/search?term=${search.value}`)
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log(response.status)
+          return;
+        }
 
+        response.json().then(function(data) {
+          let htmlstr = ``
+          data.results.forEach(function(data) {
+            htmlstr += `
+          <div class="searchresult">
+              <a href="${data.previewUrl}">
+              <img class="image" src="${data.artworkUrl100}"><br>
+              </a>
+                  <p class="title">${data.trackName}</p><br>
+                  <p class="name">${data.artistName}</p>
+             </div>
+          `
+          })
+          main.innerHTML = htmlstr
+          let albums = document.querySelectorAll('.searchresult a')
 
-// 2. Create your `submit` event for getting the user's search term
-submit.addEventListener('click', function() {
- let search = document.getElementById('search').value;
- let search url = url + search;
-
-// 3. Create your `fetch` request that is called after a submission
-fetch(searchURL)
-   .then(
-     function(response) {
-       if (response.status !== 200) {
-         console.log('Something no bueno ' + response.status);
-         return;
-
-       } console.log(searchURL);
-
-// 4. Create a way to append the fetch results to your page
-
-response.json().then(function(data) {
-      let searchResults = document.getElementById('search_results');
-        searchResults.innerHTML = "";
-      for (let i = 0; i < data.results.length; i ++) {
-         let track = data.results[i].trackName;
-         let artist = data.results[i].artistName;
-         let image = data.results[i].artworkUrl100;
-         let preview = data.results[i].previewUrl;
-         let results =
-         `
-         <img src="${thumnail}">
-         <p> class="${track}">${track}</p>
-         <p> class="${artist}">${artist}</p>
-         `
-// 5. Create a way to listen for a click that will play the song in the audio play
+          albums.forEach(function(listen) {
+            listen.addEventListener('click', function(event) {
+              event.preventDefault();
+              let url = listen.getAttribute("href");
+              document.querySelector('audio').setAttribute('src', url);
+            })
+          })
+        })
+      }
+    )
+})
